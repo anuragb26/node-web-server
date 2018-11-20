8d3525ff9564a3e6c4b046164fe8728535311a7a
Anurag Bajaj <AnuragB@talentica-all.com>
2018-11-21 00:19:03 +0530

    Chore:Adding Projects Route

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/about', (req, res) => {
   })
 })
 
+app.get('/projects', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Portfolio'
+  })
+})
+
 app.get('/bad', (req, res) => {
   res.send({ errorMessage: 'Error handling request' })
 })
